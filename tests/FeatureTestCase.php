3a705faa1eb89d0a5265245d2308cbd3a010b3c3
diego silva <diegosilva@ghostbrains.com>
2017-09-27 11:04:53 -0400

    Add feature test case

---
+++ tests/FeatureTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ASUS
+ * Date: 27/9/2017
+ * Time: 10:51
+ */
+use  \Illuminate\Foundation\Testing\DatabaseTransactions;
+class FeatureTestCase extends TestCase
+{
+   use DatabaseTransactions;
+}
